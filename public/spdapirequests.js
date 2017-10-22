1f4ff43df5dd4f35fc74e9e9918d8c7de373d3d0
Oscar Tu <o.car.w2@gmail.com>
2017-10-21 22:46:46 -0700

    Add heatmap for .json SPD crime data when zoomed out

---
+++ public/spdapirequests.js
@@ -0,0 +1,21 @@
+
+
+
+function get_crimes_url() {
+	url = "https://data.seattle.gov/resource/policereport.json"
+	return url
+}
+
+function get_crimes(callback) {
+	url = get_crimes_url()
+    let options = {
+      "url": url,
+      "method": "GET",
+      "processData": false
+    }
+    console.log("here")
+
+    $.ajax(options).done((response)=>{
+        success: callback(response)
+    })
+}
--- spdapi_requests.py
+++ spdapi_requests.py
@@ -1,3 +1,18 @@
+# Python 3
+import urllib.request, json 
+import Crime
 
+def get_crime_information():
+	with urllib.request.urlopen("https://data.seattle.gov/resource/policereport.json") as url:
+	    data = json.loads(url.read().decode())
+	    count = 0
+	    for offense in data:
+	    	count += 1
+	    	crime = Crime.Crime(offense)
+	    	print("Summarized Offense Description: " + crime.description + " Started at: " + crime.start_time + " Latitude: " + crime.lat + ", Longitude: " + crime.lng)
 
-def get_crimes():
+def main():
+	get_crime_information()
+
+if __name__ == '__main__':
+	main()
