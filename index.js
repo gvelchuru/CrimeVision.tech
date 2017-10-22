18706df4158721fe92c768dc1790439ddcb09280
mundrapranay <pranaymundra4510@gmail.com>
2017-10-22 00:30:39 -0700

    front end

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var path = require('path');
 //})
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/index2.html'));
 });
 
 app.use(express.static(path.join(__dirname, 'public')))
