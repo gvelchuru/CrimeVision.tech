7efd3eb110c04bc58aa6facd795a48e6b7220ff8
Oscar Tu <o.car.w2@gmail.com>
2017-10-22 01:46:38 -0700

    Fix heatmap and add parsers

--- spdapi_requests.py
+++ spdapi_requests.py
@@ -1,18 +1,36 @@
 # Python 3
 import urllib.request, json 
 import Crime
+import numpy as np
+from sklearn.naive_bayes import MultinomialNB
+import pymesh
 
 def get_crime_information():
+	cl = []
 	with urllib.request.urlopen("https://data.seattle.gov/resource/policereport.json") as url:
 	    data = json.loads(url.read().decode())
 	    count = 0
 	    for offense in data:
 	    	count += 1
 	    	crime = Crime.Crime(offense)
-	    	print("Summarized Offense Description: " + crime.description + " Started at: " + crime.start_time + " Latitude: " + crime.lat + ", Longitude: " + crime.lng)
+	    	cl.append(crime.get_location())	
+	    return np.array(cl)
+
 
 def main():
-	get_crime_information()
+	#crime_list = get_crime_information()
+	#print(crime_list[:10])
+	#clf = MultinomialNB()
+	X = np.random.randint(5, size=(6, 100))
+	y = np.array([1, 2, 3, 4, 5, 6])
+	print(X)
+	print(y)
+	clf = MultinomialNB()
+	print(clf)
+	clf.fit(X,y)
+	x2 = clf.predict(X[2:3])
+	print(x2)
+	#clf.fit(crime_list, crime_list[::10])
 
 if __name__ == '__main__':
 	main()
